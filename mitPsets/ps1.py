5dab0a09ff6ecb79ef46d188d2e9a15495aaa931
kuiper616 <109433742+kuiper616@users.noreply.github.com>
2023-01-03 13:50:46 -0500

    Add files via upload
    finds any given prime given a value of x

---
+++ mitPsets/ps1.py
@@ -0,0 +1,21 @@
+#hours spent = 5:00
+#name = dominic denti
+def Prime(x):
+    arr = [2]
+    i = 2
+    for n in range(1000000):
+        if(len(arr) == x):
+            #when arr is of length == to x then you have found the xth prime
+            print(arr)
+            return arr[-1]
+        for k in range(len(arr)+1):
+            if(k >= len(arr)):
+                #if k > length of array then i must be prime
+                arr.append(i)
+                i += 1
+                break
+            if(i%arr[k] == 0):
+                #if i%arr[k] == 0 then i is compesite
+                i += 1
+                break
+print(Prime(1000))
