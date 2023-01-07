6b136ece588a05b95b3a297c59cb4fd9fc2f71b6
kuiper616 <109433742+kuiper616@users.noreply.github.com>
2023-01-07 15:58:47 -0500

    Update ps1b.py

--- mitPsets/ps1b.py
+++ mitPsets/ps1b.py
@@ -10,12 +10,19 @@ def Prime(prime):
         if(len(primeArr) == prime):
             #when arr is of length == to prime then you have found the primeth prime
             return primeArr
+
         for m in range(len(primeArr)+1):
             if(m >= len(primeArr)):
                 #if m > length of array then it must be prime
                 primeArr.append(it)
                 it += 1
                 break
+
+            if(it >= 1000 and primeArr[m] > sqrt(it)):
+                primeArr.append(it)
+                it += 1
+                break
+
             if(it%primeArr[m] == 0):
                 #if it%primeArr[m] == 0 then it is compesite
                 it += 1
